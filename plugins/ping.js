6e46d89a663412cbb4deed6225dcd29c5f32b7f0
DybyTech <dybytech00@gmail.com>
2025-05-07 09:06:27 -0400

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -32,7 +32,7 @@ const ping = async (m, Matrix) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '0029VatK7Ab6buMGF3Dq6q0V@newsletter.whatsapp.net',
+          newsletterJid: '120363372853772240@newsletter',
           newsletterName: "MEGALODON-MD",
           serverMessageId: 143
         }
