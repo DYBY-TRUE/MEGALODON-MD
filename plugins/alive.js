317fd68591540cbed88b06164ebdea88b8a15f8a
DybyTech <dybytech00@gmail.com>
2025-05-07 09:14:56 -0400

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -24,7 +24,7 @@ const alive = async (m, Matrix) => {
       forwardingScore: 999,
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: '0029VatK7Ab6buMGF3Dq6q0V@newsletter',
+        newsletterJid: '120363372853772240@newsletter',
         newsletterName: "MEGALODON-MD",
         serverMessageId: 143
       }
