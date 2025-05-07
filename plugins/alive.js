3cccb4c9b1d42ecdd793323f04df7a5fc607f196
DybyTech <dybytech00@gmail.com>
2025-05-07 09:17:33 -0400

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -17,7 +17,7 @@ const alive = async (m, Matrix) => {
   const str = `*🤖 Bot Status: Online*\n*⏳ Uptime: ${timeString}*`;
 
   await Matrix.sendMessage(m.from, {
-    image: fs.readFileSync('https://files.catbox.moe/rful77.jpg'),
+    
     caption: str,
     contextInfo: {
       mentionedJid: [m.sender],
