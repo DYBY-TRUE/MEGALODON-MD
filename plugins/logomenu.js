de3f15dc8926398ef1b04403947dae90c42cc799
DybyTech <dybytech00@gmail.com>
2025-05-06 22:23:52 -0400

    Update logomenu.js

--- plugins/logomenu.js
+++ plugins/logomenu.js
@@ -56,7 +56,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363382023564830@newsletter',
+                        newsletterJid: '0029VatK7Ab6buMGF3Dq6q0V@newsletter',
                         newsletterName: "MEGALODON-MD LOGO MENU⚡₊",
                         serverMessageId: 143
                     }
